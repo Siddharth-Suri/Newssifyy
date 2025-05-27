ea080cb0dabee8878fe667c93bf4349b05800cff
Siddharth Suri <siddharth2005suri@gmail.com>
2025-05-27 13:01:30 +0530

    name change

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,17 @@
 export function TopBar() {
     return (
         <div className="flex justify-center items-center bg-black p-4">
-            <div className="w-3xl">
+            <div className="w-4xl">
                 <div className="flex p-2 justify-between items-center ">
-                    <div className="flex gap-2 text-2xl text-amber-500  font-medium text-center item-center cursor-pointer">
-                        <div className="overline ">The</div>
+                    <div
+                        className="flex  text-2xl text-amber-500  font-medium text-center item-center cursor-pointer"
+                        onClick={() => {
+                            window.location.reload()
+                        }}
+                    >
+                        <div className=" text-white">News</div>
 
-                        <span className="text-amber-50">News</span>
+                        <div className="">sifyy</div>
                     </div>
                     <div className="text-amber-500 p-2 text-center cursor-pointer item-center underline">
                         github
