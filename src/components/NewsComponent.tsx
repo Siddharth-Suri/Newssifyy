5fd277e119000ab48cc50ae28117bc61a3e3f84d
Siddharth Suri <siddharth2005suri@gmail.com>
2025-05-27 13:28:25 +0530

    ui changes

--- src/components/NewsComponent.tsx
+++ src/components/NewsComponent.tsx
@@ -3,31 +3,31 @@ import { topStoriesType } from "../utils/api"
 
 export function NewsComponent(props: topStoriesType) {
     return (
-        <div className="p-2 border-b-2 mb-2 border-neutral-800 container mx-auto text-white bg-black  hover:opacity-95 hover:rounded-2xl hover:bg-neutral-900 hover:pt-4  transition-all duration-200">
-            <a
-                href={props.url}
-                target="_blank"
-                className="hover:text-amber-500 hover:underline"
-            >
-                <div className="flex flex-wrap p-2 font-bold text-lg w-4xl ">
-                    {props.title}
+        <div className="p-2 border-b-2 mb-2 border-neutral-800 container mx-auto text-neutral-100 bg-black  hover:opacity-95 hover:rounded-2xl hover:bg-neutral-900 hover:pt-4  transition-all duration-200">
+            <div className="flex flex-wrap p-2 font-bold text-lg w-4xl ">
+                <div className="hover:text-amber-500 hover:underline">
+                    <a href={props.url} target="_blank" className="">
+                        {props.title}
+                    </a>
                 </div>
-            </a>
-            <div className="flex text-sm gap-1 p-2">
+            </div>
+            <div className="flex text-sm gap-1 p-2 text-gray-400">
                 <div>By: </div>
                 <div className="cursor-pointer underline text-amber-500">
                     {props.by}
                 </div>
             </div>
-            <div className="flex text-sm">
-                <div className="p-2 cursor-pointer hover:underline text-amber-500">
+            <div className="flex text-sm text-gray-400">
+                <div className="p-2 cursor-pointer hover:underline text-amber-500 ">
                     {props.descendants} comments
                 </div>
-                <div className="flex gap-1 p-2 items-center">
+                <div className="flex gap-1 p-2 items-center text-gray-400">
                     <StarIcon></StarIcon>
                     <span>{props.score}</span>
                 </div>
-                <div className="p-2">{props.time}</div>
+                <div className="p-2 text-gray-400">
+                    {new Date(props.time * 1000).toLocaleDateString()}
+                </div>
             </div>
         </div>
     )
